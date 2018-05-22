d11d31fc4a0808a43dbae75df52d523b15dc1104
jharmon8 <jharmon@caltech.edu>
2018-05-22 05:10:48 -0700

    Finish a good first day of work

--- src/graphics/PewPanel.java
+++ src/graphics/PewPanel.java
@@ -44,7 +44,7 @@ public class PewPanel extends JPanel implements KeyListener, ActionListener {
 
     private int extraDummy = -1;
 
-    private final boolean SKIP_TO_GAME = true;
+    private final boolean SKIP_TO_GAME = false;
 
     public PewPanel () {
         if(simulateProjectorAspectRatio) {
